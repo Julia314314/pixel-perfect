5aba2284be10169e86a8a77f9403ac2ddb8480c9
Julia314314 <chenpengxin47@gmail.com>
2025-12-16 23:15:37 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import PostPage from "./pages/PostPage";
 import AIEngineering from "./pages/AIEngineering";
@@ -23,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/post/:slug" element={<PostPage />} />
@@ -36,12 +37,12 @@ const App = () => (
           <Route path="/subscribe" element={<Subscribe />} />
           <Route path="/archive" element={<Archive />} />
           <Route path="/newsletter/:slug" element={<NewsletterPage />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
 
 export default App;
+
